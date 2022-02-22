33c7e007bed150db3bcf6bfad6ee7480e818c8a1
Seongil Yoon <dbs3346@gmail.com>
2022-02-22 16:45:44 +0900

    게시판목록1.1

--- WebContent/js/freeboard_list.js
+++ WebContent/js/freeboard_list.js
@@ -24,37 +24,38 @@ $(document).ready(function () {
     ////////////////////////////////
 
     function createData() {
-        boardList.push(new Board(0, "글0", "윤성일0", "안녕하세요0"));
-        boardList.push(new Board(1, "글1", "윤성일1", "안녕하세요1"));
-        boardList.push(new Board(2, "글2", "윤성일2", "안녕하세요2"));
-        boardList.push(new Board(3, "글3", "윤성일3", "안녕하세요3"));
-        boardList.push(new Board(4, "글4", "윤성일4", "안녕하세요4"));
-        boardList.push(new Board(5, "글5", "윤성일5", "안녕하세요5"));
-        boardList.push(new Board(6, "글6", "윤성일6", "안녕하세요6"));
-        boardList.push(new Board(7, "글7", "윤성일7", "안녕하세요7"));
-        boardList.push(new Board(8, "글8", "윤성일8", "안녕하세요8"));
-        boardList.push(new Board(9, "글9", "윤성일9", "안녕하세요9"));
+        boardMap.set(0, new Board(0, "글0", "윤성일0", "안녕하세요0"))
+        boardMap.set(1, new Board(1, "글1", "윤성일1", "안녕하세요1"));
+        boardMap.set(2, new Board(2, "글2", "윤성일2", "안녕하세요2"));
+        boardMap.set(3, new Board(3, "글3", "윤성일3", "안녕하세요3"));
+        boardMap.set(4, new Board(4, "글4", "윤성일4", "안녕하세요4"));
+        boardMap.set(5, new Board(5, "글5", "윤성일5", "안녕하세요5"));
+        boardMap.set(6, new Board(6, "글6", "윤성일6", "안녕하세요6"));
+        boardMap.set(7, new Board(7, "글7", "윤성일7", "안녕하세요7"));
+        boardMap.set(8, new Board(8, "글8", "윤성일8", "안녕하세요8"));
+        boardMap.set(9, new Board(9, "글9", "윤성일9", "안녕하세요9"));
     }
 
     function creatView() {
 
         $(".boardLine").remove();
         let html = '';
-        for (i in boardList) {
+        // map순회
+        for (let amount of boardMap.values()) {
             html += `
-                <li class="boardLine" value="${boardList[i].seq}">
+                <li class="boardLine" value="${amount.seq}">
                     <div>
-                        <a class="contents" id="boardInfo" name="boardInfo" href=board?board_num=${boardList[i].seq}>
-                            <span>${boardList[i].seq}</span>
-                            <span>${boardList[i].title}</span>
-                            <span>${boardList[i].contents}</span>
-                            <span>${boardList[i].writer}</span>
-                            <span>${boardList[i].time}</span>
-                            <span>${boardList[i].viewcount}</span>
+                        <a class="contents" id="boardInfo" name="boardInfo" href=board?freeBoard_num=${amount.seq}>
+                            <span>${amount.seq}</span>
+                            <span>${amount.title}</span>
+                            <span>${amount.contents}</span>
+                            <span>${amount.writer}</span>
+                            <span>${amount.time}</span>
+                            <span>${amount.viewcount}</span>
                         </a>
                     </div>
                     <div>
-                        <a class="btnClass" id="alterBtn" name="alterBtn" href=alterboard?board_num=${boardList[i].seq}>
+                        <a class="btnClass" id="alterBtn" name="alterBtn" href=alterboard?freeBoard_num=${amount.seq}>
                             글 수정
                         </a>
                         <a class="btnClass" id="delBtn" name="delBtn";">
@@ -63,7 +64,6 @@ $(document).ready(function () {
                     </div>
                 </li>
             `
-            console.log(boardList[i].title);
         }
         $("ul[class=boardList]").append(html);
 
@@ -72,19 +72,18 @@ $(document).ready(function () {
     function deleteHandler() {
         $(document).on("click", "#delBtn", function (event) {
             //이벤트 부모태그인 li의 value값 가져오기
-            let board_num = $(event.target).parents(".boardLine").val();
+            let freeBoard_num = $(event.target).parents(".boardLine").val();
 
             // 객체의 seq값을 찾아서 없애야 됨
-            boardList.splice(board_num, 1)
-            console.log(boardList);
-
+            boardMap.delete(freeBoard_num);
+            console.log(boardMap);
             creatView();
         }); //end of click
     }
 
     function main() {
         createData();
-        console.log(boardList);
+        console.log(boardMap);
         creatView();
         deleteHandler();
 
--- WebContent/js/qustionboard_list.js
+++ WebContent/js/qustionboard_list.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
 
-
-
-    let boardList = [];
+    // let boardList = [];
+    let boardMap = new Map();
 
     class Board {
         constructor(seq, title, writer, contents) {
@@ -25,63 +24,67 @@ $(document).ready(function () {
     ////////////////////////////////
 
     function createData() {
-        boardList.push(new Board(1, "첫번째 글", "윤성일1", "안녕하세요1"));
-        boardList.push(new Board(2, "두번째 글", "윤성일2", "안녕하세요2"));
-        boardList.push(new Board(3, "세번째 글", "윤성일3", "안녕하세요3"));
-        boardList.push(new Board(4, "네번째 글", "윤성일4", "안녕하세요4"));
-        boardList.push(new Board(5, "다섯번째 글", "윤성일5", "안녕하세요5"));
-        boardList.push(new Board(6, "여섯번째 글", "윤성일6", "안녕하세요6"));
-        boardList.push(new Board(7, "일곱번째 글", "윤성일7", "안녕하세요7"));
-        boardList.push(new Board(8, "여덟번째 글", "윤성일8", "안녕하세요8"));
-        boardList.push(new Board(9, "아홉번째 글", "윤성일9", "안녕하세요9"));
-        boardList.push(new Board(10, "열번째 글", "윤성일10", "안녕하세요10"));
+        boardMap.set(0, new Board(0, "글0", "윤성일0", "안녕하세요0"))
+        boardMap.set(1, new Board(1, "글1", "윤성일1", "안녕하세요1"));
+        boardMap.set(2, new Board(2, "글2", "윤성일2", "안녕하세요2"));
+        boardMap.set(3, new Board(3, "글3", "윤성일3", "안녕하세요3"));
+        boardMap.set(4, new Board(4, "글4", "윤성일4", "안녕하세요4"));
+        boardMap.set(5, new Board(5, "글5", "윤성일5", "안녕하세요5"));
+        boardMap.set(6, new Board(6, "글6", "윤성일6", "안녕하세요6"));
+        boardMap.set(7, new Board(7, "글7", "윤성일7", "안녕하세요7"));
+        boardMap.set(8, new Board(8, "글8", "윤성일8", "안녕하세요8"));
+        boardMap.set(9, new Board(9, "글9", "윤성일9", "안녕하세요9"));
     }
 
-
     function creatView() {
+
+        $(".boardLine").remove();
         let html = '';
-        for (i in boardList) {
+        // map순회
+        for (let amount of boardMap.values()) {
             html += `
-                <li class="boardLine">
-                    <a>
-                        <span>${boardList[i].seq}</span>
-                        <span>${boardList[i].title}</span>
-                        <span>${boardList[i].contents}</span>
-                        <span>${boardList[i].writer}</span>
-                        <span>${boardList[i].time}</span>
-                        <span>${boardList[i].viewcount}</span>
-                        <span>
-                            <button class="boardLineBtn" id="boardAlterBtn">글 수정</button>
-                        </span>
-                        <span>
-                            <button class="boardLineBtn" id="boardDeleteBtn">글 삭제</button>
-                        </span>
-                    </a>
+                <li class="boardLine" value="${amount.seq}">
+                    <div>
+                        <a class="contents" id="boardInfo" name="boardInfo" href=board?quesBoard_num=${amount.seq}>
+                            <span>${amount.seq}</span>
+                            <span>${amount.title}</span>
+                            <span>${amount.contents}</span>
+                            <span>${amount.writer}</span>
+                            <span>${amount.time}</span>
+                            <span>${amount.viewcount}</span>
+                        </a>
+                    </div>
+                    <div>
+                        <a class="btnClass" id="alterBtn" name="alterBtn" href=alterboard?quesBoard_num=${amount.seq}>
+                            질문 수정
+                        </a>
+                        <a class="btnClass" id="delBtn" name="delBtn";">
+                            질문 삭제
+                        </a>
+                    </div>
                 </li>
             `
-            console.log(boardList[i].title);
         }
         $("ul[class=boardList]").append(html);
 
     }
 
-    function alterHandler() {
-        $("#boardAlterBtn").on("click", function () {
-            console.log("ASd");
-        })
-    }
-
     function deleteHandler() {
-        $("#boardDeleteBtn").on("click", function () {
+        $(document).on("click", "#delBtn", function (event) {
+            //이벤트 부모태그인 li의 value값 가져오기
+            let quesBoard_num = $(event.target).parents(".boardLine").val();
 
-        })
+            // 객체의 seq값을 찾아서 없애야 됨
+            boardMap.delete(quesBoard_num);
+            console.log(boardMap);
+            creatView();
+        }); //end of click
     }
 
     function main() {
         createData();
-        console.log(boardList);
+        console.log(boardMap);
         creatView();
-        alterHandler();
         deleteHandler();
 
     }
