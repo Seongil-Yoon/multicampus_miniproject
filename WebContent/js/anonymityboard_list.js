43cc51190bebd48ffba2259bb5896ed17f32e656
Seongil Yoon <dbs3346@gmail.com>
2022-02-23 17:10:40 +0900

    머지전

--- WebContent/js/anonymityboard_list.js
+++ WebContent/js/anonymityboard_list.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
       this.title = title;
       this.writer = writer;
       this.contents = contents;
-      this.time = new Date().toLocaleString();
+      this.time = new Date().toLocaleTimeString();
       this.viewcount = 0;
     }
     toString() {
@@ -57,6 +57,7 @@ $(document).ready(function () {
                             <span>${amount.contents}</span>
                             <span>${amount.writer}</span>
                             <span>${amount.time}</span>
+                            <!--<span>${timeForToday(amount.time)}</span>-->
                             <span>${amount.viewcount}</span>
                         </a>
                     </div>
@@ -77,14 +78,58 @@ $(document).ready(function () {
 
   function deleteHandler() {
     $(document).on("click", "#delBtn", function (event) {
-      //이벤트 부모태그인 li의 value값 가져오기
-      let freeBoard_num = $(event.target).parents(".boardLine").val();
+      swal({
+          title: "다시 확인해 주세요",
+          text: "콘텐츠를 삭제하면 다시 복구가 불가능합니다",
+          icon: "warning",
+          buttons: true,
+          dangerMode: true,
+        })
+        .then((willDelete) => {
+          if (willDelete) {
+            //이벤트 부모태그인 li의 value값 가져오기
+            let freeBoard_num = $(event.target).parents(".boardLine").val();
 
-      // 객체의 seq값을 찾아서 없애야 됨
-      boardMap.delete(freeBoard_num);
-      console.log(boardMap);
-      creatView();
+            // 객체의 seq값을 찾아서 없애야 됨
+            boardMap.delete(freeBoard_num);
+            console.log(boardMap);
+            creatView();
+
+          } else {
+            swal("취소하였습니다.");
+          }
+        });
     }); //end of click
+  }
+
+  //시간차이 계산 함수
+  function timeForToday(value) {
+    const today = new Date();
+    const timeValue = new Date(value);
+    console.log(today.getTime());
+
+    //시간은 1970-01-01을 기준으로 한 에포크시간.
+    const betweenTime = Math.floor((today.getTime() - timeValue.getTime()) / 1000 / 60);
+
+    if (betweenTime < 1) return '방금전';
+    if (betweenTime < 60) {
+      return `${betweenTime}분전`;
+    }
+
+    const betweenTimeHour = Math.floor(betweenTime / 60);
+    if (betweenTimeHour < 24) {
+      return `${betweenTimeHour}시간전`;
+    }
+
+    const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+    if (betweenTimeDay < 365) {
+      if (betweenTimeDay == 0) {
+        return `오늘 작성`;
+      }
+      return `${betweenTimeDay}일전 작성`;
+    }
+
+    return `${Math.floor(betweenTimeDay / 365)}년전`;
   }
 
   function main() {
--- WebContent/js/mypage.js
+++ WebContent/js/mypage.js
@@ -1,10 +1,8 @@
 window.onload = function () {
-  var button = document.getElementById("infochange");
-  button.onclick = function () {
-    alert("미구현 기능입니다. 기다려주세요.");
-  };
-  var button = document.getElementById("pwchange");
-  button.onclick = function () {
-    alert("미구현 기능입니다. 기다려주세요.");
-  };
+  var button1 = $(document).on("click", "#infochange", function (event) {
+    swal("미구현 기능입니다!", "업데이트를 기다려주세요.", "error");
+  });
+  var button2 = $(document).on("click", "#pwchange", function (event) {
+    swal("미구현 기능입니다!", "업데이트를 기다려주세요.", "error");
+  });
 };
